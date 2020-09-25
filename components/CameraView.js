0b2419ecf4a5e22a9e99c58596d6447c22a999ef
user86 <kshitij.kotasthane@gmail.com>
2020-09-25 10:57:23 +0530

    Added switching cameras (front/back)

--- components/CameraView.js
+++ components/CameraView.js
@@ -1,20 +1,37 @@
 import { Camera } from "expo-camera";
 import * as Permissions from "expo-permissions";
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
+import {
+  FontAwesome,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
 // TODO: Hide status bar in camera view
 
 export default class CameraView extends Component {
   state = {
     hasPermission: null,
-    type: Camera.Constants.Type.Back,
+    cameraType: Camera.Constants.Type.front,
   };
 
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasPermission: status === "granted" });
   }
+
+  handleCameraType = () => {
+    const { cameraType } = this.state;
+
+    this.setState({
+      cameraType:
+        cameraType === Camera.Constants.Type.front
+          ? Camera.Constants.Type.back
+          : Camera.Constants.Type.front,
+    });
+  };
+
   render() {
     const { hasPermission } = this.state;
     if (hasPermission === null) {
@@ -24,7 +41,61 @@ export default class CameraView extends Component {
     } else {
       return (
         <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.cameraType}></Camera>
+          <Camera
+            style={{ flex: 1, alignContent: "flex-end" }}
+            type={this.state.cameraType}
+          >
+            <View
+              opacity={0.5}
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                margin: 20,
+                backgroundColor: "transparent",
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  alignSelf: "flex-end",
+                  alignItems: "center",
+                  backgroundColor: "transparent",
+                }}
+              >
+                <Ionicons
+                  name="ios-photos"
+                  style={{ color: "#fff", fontSize: 40 }}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  alignSelf: "flex-end",
+                  alignItems: "center",
+                  backgroundColor: "transparent",
+                }}
+              >
+                <FontAwesome
+                  name="camera"
+                  style={{ color: "#fff", fontSize: 40 }}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  alignSelf: "flex-end",
+                  alignItems: "center",
+                  backgroundColor: "transparent",
+                }}
+                onPress={() => {
+                  this.handleCameraType();
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="camera-switch"
+                  style={{ color: "#fff", fontSize: 40 }}
+                />
+              </TouchableOpacity>
+            </View>
+          </Camera>
         </View>
       );
     }
